6c29390fafc40efac43855aa08088d0671b866b4
rehan700 <91946977+rehan700@users.noreply.github.com>
2021-10-25 10:31:35 +0530

    Update CheckForPrime.c

--- C/CheckForPrime.c
+++ C/CheckForPrime.c
@@ -6,7 +6,7 @@ int main() {
   printf("Enter a positive integer: ");
   scanf("%d", &n);
 
-  for (i = 2; i <= n / 2; ++i) {
+  for (i = 2; i <= n / 2+1; ++i) {
     // condition for non-prime
     if (n % i == 0) {
       flag = 1;
